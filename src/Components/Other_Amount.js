bdcdfddf57d51a8549749f05ea43dd43daed8778
olawale-hammed <olawale.h.w@gmail.com>
2019-07-15 08:51:57 +0100

    Ready for heroku

--- src/Components/Other_Amount.js
+++ src/Components/Other_Amount.js
@@ -1,28 +1,42 @@
 import React from 'react'
 import './Account_Number.css'
-import { BrowserRouter as Router, Link, Redirect} from 'react-router-dom';
-import Route from 'react-router-dom/Route';
+import { BrowserRouter as Router, Link, Redirect} from 'react-router-dom'
+import Route from 'react-router-dom/Route'
 import Login from '../index'
-import Message from './Message';
+import Message from './Message'
+import Message2 from './Message2'
 
 
 class Other_Amount extends React.Component{
 	constructor(props){
 		super(props)
-		this.state = {input: null, Debit: localStorage.getItem('Amount'), status: false}
+		this.state = {input: '', Debit: localStorage.getItem('Amount'), status: false,
+		 error: 'Input value must be four digits and cannot be empty', amount: null}
 		this.inputRef = React.createRef()
 	}
 	componentDidMount(){
 		this.inputRef.current.focus()
 	}
 	handleChange = (e) => {
-		this.setState({input: e.target.value})
+		this.setState({input: e.target.value, amount: e.target.value})
 	}
 	handleClick = () => {
-		this.setState({Debit: localStorage.getItem('Amount') - this.state.input, status: true})
+		this.setState({Debit: localStorage.getItem('Amount') - this.state.input})
+		if(this.state.input.length > 5 || isNaN(this.state.input) || this.state.input === ''){
+			document.getElementById('demo').innerHTML = this.state.error
+		}else{
+			this.setState({status: true})
+		}
+		
+		
 	}
-	handle_Redirect = () => {
-		localStorage.Amount = this.state.Debit
+	handleRedirect = () => {
+		// localStorage.Amount = this.state.Debit
+		if(this.state.amount <= localStorage.getItem('Amount')){
+			localStorage.Amount = this.state.Debit
+		}else{
+			return <Redirect to='/message2' />
+		}
 		if(this.state.status){
 			return <Redirect to='/message' />
 		}
@@ -34,9 +48,10 @@ class Other_Amount extends React.Component{
 				<Route path='/others' exact strict render={() => {
 					return(
 						<div id="container" className='diff'>
-							{this.handle_Redirect()}
+							{this.handleRedirect()}
 							<h1 > Other Amount </h1>
-							<input id='in' type='text' ref={this.inputRef} onChange={this.handleChange} value={this.input} size='5' maxLength='5'/>
+							<h4 id='demo'> </h4>
+							<input id='in' type='text' ref={this.inputRef} onChange={this.handleChange} value={this.input} size='7' maxLength='7'/>
 							<div>
 								<button id='bt' onClick={this.handleClick}> PROCEED </button><br /><br /><br />
 								<button id='bt'><Link to='/' className='effect' > EXIT </Link></button>                              	
@@ -46,6 +61,7 @@ class Other_Amount extends React.Component{
                 }} />
 				<Route path="/" exact strict component={Login} />
 				<Route path="/message" exact strict component={Message} />
+				<Route path="/message2" exact strict component={Message2} />
 			</div>
 		</Router>
 	)
